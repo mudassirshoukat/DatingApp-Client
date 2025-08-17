a43a211877539217b19f0200a2c9d6badd03ebd6
Mudassir Shoukat <mudassirshoukat7577@gmail.com>
2025-08-17 15:40:50 +0500

    Package corrected

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
-    production:false,
-   
+    production: false,
+
     // ApiUrl:"https://localhost:44320/api/",
     // hubUrl:"https://localhost:44320/hubs/",
- ApiUrl:"https://app-datingapp-api-cc-dev-001.azurewebsites.net/api/",
-    hubUrl:"https://app-datingapp-api-cc-dev-001.azurewebsites.net/hubs/",
+    ApiUrl: "https://datingapp-dev-01-cc.azurewebsites.net/api/",
+    hubUrl: "https://datingapp-dev-01-cc.azurewebsites.net/hubs/",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,4 @@ export const environment = {
     production:true,
     ApiUrl: "https://app-datingapp-api-cc-dev-001.azurewebsites.net/api/",
     hubUrl: "https://app-datingapp-api-cc-dev-001.azurewebsites.net/hubs/",
-
-
 };
